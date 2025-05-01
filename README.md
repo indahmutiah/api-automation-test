2e4eda9ceec7c65fa7206446b8e6b168f390fc5a
Indah Mutiah MZ <indahmutiahmz@gmail.com>
2025-05-01 10:28:58 +0700

    feat: install package and create e2e for  aut api and create booking

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# api-automation-test
-This is a repository of API Automation Testing with Mocha Chai 
+# Booker API Automation Testing
+
+This is a repository of E2E testing for the Booker API using Mocha, Chai, and Supertest.
+API Reference: [Booker API](https://restful-booker.herokuapp.com/apidoc/index.html)
+
+## Installation
+
+1. Install Mocha, Chai, and Supertest
+
+```bash
+npm install mocha chai supertest
+```
+
+2. Install Dotenv
+
+```bash
+npm install dotenv
+```
+
+## Environment Variables
+
+Create a `.env` file in the root directory of the project and add the following variables:
+
+- API_URL
+- API_BACKEND_USERNAME
+- API_BACKEND_PASSWORD
+
+## Running the tests
+
+```bash
+npx mocha booker-e2e.spec.js
+```
---
+++ booker-e2e.spec.js
@@ -0,0 +1,41 @@
+import { expect } from "chai";
+import request from "supertest";
+import dotenv from "dotenv";
+
+import data from "./data.json" assert { type: "json" };
+
+dotenv.config({ path: ".env" });
+
+let token;
+
+describe("Booker API e2e testing", function () {
+  before(async function () {
+    const body = {
+      username: process.env.API_BACKEND_USERNAME,
+      password: process.env.API_BACKEND_PASSWORD,
+    };
+    const response = await request(process.env.API_URL)
+      .post("/auth")
+      .set({ "Content-Type": "application/json", Accept: "application/json" })
+      .send(body);
+    expect(response.statusCode).to.equal(200);
+    token = response.body.token;
+    console.log("token:", token);
+  });
+
+  it("API Auth should return a valid token", async function () {
+    expect(token).to.be.a("string");
+    expect(token.length).to.be.greaterThan(0);
+  });
+
+  it("API Create Booking", async function () {
+    const response = await request(process.env.API_URL)
+      .post("/booking")
+      .set({ "Content-Type": "application/json", Accept: "application/json" })
+      .send(data);
+    expect(response.statusCode).to.equal(200);
+    expect(response.body.booking.firstname).to.equal(data.firstname);
+    expect(response.body.booking.lastname).to.equal(data.lastname);
+    console.log("response booking id:", response.body);
+  });
+});
